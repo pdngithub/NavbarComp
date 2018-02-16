118260dc983e85c5c54cca4cd10b63c6b9298b9e
jrv <jrv@urania>
2018-02-16 12:46:50 -0500

    added exploratory code to figure out which lifecycle event should be used for updating height. updated re-sizing code based on exploratory discoveries

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { NavBarItem } from '../data/nav-bar-item';
 import { Event } from '@angular/router/src/events';
 import * as $ from 'jquery';
@@ -8,53 +8,95 @@ import * as $ from 'jquery';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit, AfterViewChecked {
+// exploratory code only: remove all implements except AfterViewChecked in your code
+export class NavBarComponent implements OnInit, AfterViewChecked, AfterContentChecked, AfterContentInit, AfterViewInit {
 
-  public navBarItems: NavBarItem[] = 
-  [
-    {
-      title: "Home",
-      path: ""
-    },
-    {
-      title: "Beauty",
-      path: "beauty"
-    },
-    {
-      title: "Charm",
-      path: "charm"
-    },
-    {
-      title: "Down",
-      path: "down"
-    },
-    {
-      title: "Strange",
-      path: "strange"
-    },
-    {
-      title: "Top",
-      path: "top"
-    },
-    {
-      title: "Up",
-      path: "up"
-    }
-  ]
+  public navBarItems: NavBarItem[] =
+    [
+      {
+        title: "Home",
+        path: ""
+      },
+      {
+        title: "Beauty",
+        path: "beauty"
+      },
+      {
+        title: "Charm",
+        path: "charm"
+      },
+      {
+        title: "Down",
+        path: "down"
+      },
+      {
+        title: "Strange",
+        path: "strange"
+      },
+      {
+        title: "Top",
+        path: "top"
+      },
+      {
+        title: "Up",
+        path: "up"
+      }
+    ]
 
 
   constructor() { }
 
-  ngOnInit() {
+  // exploratory code only: remove this function in your code
+  private logStep(step: string) {
+    console.log(step + " navBarHeight = " + this.getNavbarHeight().toString());
   }
 
-  onResize( event: Event ) {
-    $(".godown-navbar").height($(".navbar-fixed-top").height());
+  // exploratory code only: remove this function in your code
+  ngOnInit() {
+    this.logStep("ngOnInit");
+  }
+
+  // exploratory code only: remove this function in your code
+  public ngAfterContentChecked() {
+    this.logStep("ngAfterContentChecked");
+  }
+
+  private getNavbarHeight(): number {
+    return $(".navbar-fixed-top").height();
+  }
+
+  // this is called to force a call to ngAfterViewChecked. 
+  // ngAfterViewChecked (and ngAfterContentChecked) 
+  // is called during the initialization sequence and after
+  // an onResize call after a window:resize event, 
+  // so that is the place to resize the navbar spacer. If 
+  // the navbar spacer is re-sized here, it will be re-sized
+  // again in ngAfterViewChecked, so this is a null implementation.
+  // The null implementation is necessary because if the event is
+  // not handled, ngAfterViewChecked is not called.
+  onResize(event: Event) {
+    // exploratory code only: remove this line in your code
+    console.log("onResize event/call");
   };
-  
+
+  private resizeGodownNavbar(): void {
+    $(".godown-navbar").height(this.getNavbarHeight);
+  }
+
+  // exploratory code only: remove this function in your code
+  ngAfterContentInit() {
+    this.logStep("ngAfterContentInit");
+  }
+
+  // exploratory code only: remove this function in your code
+  ngAfterViewInit() {
+    this.logStep("ngAfterViewInit");
+  }
 
   ngAfterViewChecked() {
-    this.onResize( null );
+    // exploratory code only: remove this line in your code
+    this.logStep("ngAfterViewChecked");
+    this.resizeGodownNavbar();
   }
 
 }
